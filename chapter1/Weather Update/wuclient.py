14c7b61fcfa0e9cc715aae25e0f9e67abc99b5d1
Chris YT Huang <ythuang@gmail.com>
2012-06-29 17:01:44 +0800

    restructure folder

---
+++ chapter1/Weather Update/wuclient.py
@@ -0,0 +1,31 @@
+#
+#   Weather update client
+#   Connects SUB socket to tcp://localhost:5556
+#   Collects weather updates and finds avg temp in zipcode
+#
+
+import sys
+import zmq
+
+#  Socket to talk to server
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+
+print "Collecting updates from weather server..."
+socket.connect ("tcp://localhost:5556")
+
+# Subscribe to zipcode, default is NYC, 10001
+zip_filter = sys.argv[1] if len(sys.argv) > 1 else "10001"
+socket.setsockopt(zmq.SUBSCRIBE, zip_filter)
+
+# Process 5 updates
+total_temp = 0
+for update_nbr in range (5):
+    string = socket.recv()
+    print string
+    (zip,temperature,relhumidity) = string.split()
+    print "zip:", zip, " temp:", temperature, " relhumidity:", relhumidity
+    total_temp += int(temperature)
+
+print "Average temperature for zipcode '%s' was %dF" % (
+    zip_filter, total_temp / update_nbr)
